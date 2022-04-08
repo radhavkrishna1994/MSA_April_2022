6f3ef056a07c4136894520232ca03f9b25cd39a7
radha <radhavkrishna1994@gmail.com>
2022-04-08 13:03:38 +0530

    updated

--- spring-boot-book-producer/src/main/java/com/training/controllers/BookController.java
+++ spring-boot-book-producer/src/main/java/com/training/controllers/BookController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.training.model.Book;
 import com.training.services.BookServiceI;
 
--- spring-boot-book-producer/src/main/java/com/training/model/Book.java
+++ spring-boot-book-producer/src/main/java/com/training/model/Book.java
@@ -1,7 +1,13 @@
 package com.training.model;
 
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,9 +21,12 @@ public class Book {
 
 	@Id  //primary key
 	private Long isbn;
+	
+	@Column(name="book_title",length = 100,nullable = false)
 	private String title;
 	private Double price;
 	private Long stock;
+	private LocalDate dop;
 	
 	
 }
