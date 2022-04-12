bb09ba431c846e51f6c1299276e33a58655c470f
radha <radhavkrishna1994@gmail.com>
2022-04-12 13:02:21 +0530

    added

--- spring-boot-security-demo/src/main/java/com/training/config/SecurityConfigurationDb.java
+++ spring-boot-security-demo/src/main/java/com/training/config/SecurityConfigurationDb.java
@@ -1,27 +1,49 @@
 package com.training.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfigurationInMemory extends WebSecurityConfigurerAdapter {
+import com.training.services.MyUserDetailsService;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurationDb extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder myPasswordEncoder()
+	{
+		return new PasswordEncoder() {
+
+			@Override
+			public String encode(CharSequence rawPassword) {
+				
+				return rawPassword.toString();
+			}
+
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				
+				return rawPassword.equals(encodedPassword);
+			}
+			
+		};
+	}
+	
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+	
 	//Authentication
 	public void configure(AuthenticationManagerBuilder auth) throws Exception
 	{
 		auth
-		.inMemoryAuthentication()   //UserDetailsService
-		.withUser("user1")
-		.password("{noop}user123")
-		.roles("USER")
-		.and()
-		.withUser("admin")
-		.password("{noop}admin123")
-		.roles("ADMIN");
+		.userDetailsService(userDetailsService)
+		.passwordEncoder(myPasswordEncoder());
 	}
 
 	// Authorization
