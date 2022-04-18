9109ccf028960192875b89cfc2a0604d902f53bb
radha <radhavkrishna1994@gmail.com>
2022-04-18 12:29:57 +0530

    Update TestAspect.java

--- spring-boot-aop-demo/src/main/java/com/training/aspects/TestAspect.java
+++ spring-boot-aop-demo/src/main/java/com/training/aspects/TestAspect.java
@@ -1,6 +1,10 @@
 package com.training.aspects;
 
+import java.time.LocalDate;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -20,6 +24,29 @@ public class TestAspect {
 		String className = joinPoint.getTarget().getClass().toString();
 		log.info(className);
 		log.info(""+joinPoint.getSignature());
+		
+	}
+	
+	@Around(value="execution(* com.training.services.AOPService.get*())")
+	public Object logAround(ProceedingJoinPoint joinPoint)
+	{
+		log.info("This is start of around advice");
+		log.info("ClassName :"+joinPoint.getTarget().getClass().toString());
+		log.info(""+joinPoint.getSignature());
+		
+		log.info(LocalDate.now()+"");
+		
+		Object value="";
+		
+		try {
+			value = joinPoint.proceed();
+			log.info("Value inside the method="+value);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		log.info("Back to Aspect");
+		return value;
 		
 	}
 	
