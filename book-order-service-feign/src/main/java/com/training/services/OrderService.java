da2b65c89bd55a5c8a069d61b087114e8e73e6b4
radha <radhavkrishna1994@gmail.com>
2022-04-20 10:34:55 +0530

    Update OrderService.java

--- book-order-service-feign/src/main/java/com/training/services/OrderService.java
+++ book-order-service-feign/src/main/java/com/training/services/OrderService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.training.config.OrderConfiguration;
+
 import com.training.model.OrderedBook;
 import com.training.proxy.OrderFeign;
 
